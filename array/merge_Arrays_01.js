9cfcd1192643206105f76b035d6607da9f8e6af3
shubham78 <sharaff0shubham@gmail.com>
2023-01-11 14:26:03 +0530

    Merge Array Program

---
+++ array/merge_Arrays_01.js
@@ -0,0 +1,18 @@
+
+function mergeArray(arr1, arr2) {
+    let arr = []
+    for (i = 0; i < arr1.length; i++) {
+        arr[i] = arr1[i]
+    }
+    for (i = 0; i < arr2.length; i++) {
+        arr[arr1.length + i] = arr2[i]
+        // arr[arr.length] = arr2[i] 
+        // It will also give same output
+    }
+    return arr
+}
+
+let arr1 = [1, 2, 3]
+let arr2 = [10, 20, 30]
+let output = mergeArray(arr1, arr2);
+console.log(output)
